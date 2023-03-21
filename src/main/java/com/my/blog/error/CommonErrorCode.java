09f59b3a837254eeb3083b2d017d9ebda3ff3492
minwestgit <minwest61@gmail.com>
2023-03-22 00:54:57 +0900

    ErrorCode 메시지 수정

--- src/main/java/com/my/blog/error/CommonErrorCode.java
+++ src/main/java/com/my/blog/error/CommonErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "유효하지 않은 파라미터가 포함되어 있습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error가 발생했습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버에 오류가 발생했습니다."),
     ;
 
     private final HttpStatus httpStatus;
